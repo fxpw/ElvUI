37beb7a158d425ebd9ca0df55521a4efb73c163a
fxpw <fxpw@outlook.com>
2022-07-21 01:40:57 +0300

    фикс фикста

--- ElvUI/Modules/UnitFrames/Elements/HeadHuntingWanted_Sirus.lua
+++ ElvUI/Modules/UnitFrames/Elements/HeadHuntingWanted_Sirus.lua
@@ -33,7 +33,44 @@ local event = CreateFrame("Frame")
 -- do
 -- 	event:OnLoad()
 -- end
+
 event:RegisterEvent("CHAT_MSG_ADDON")
+event:SetScript("OnEvent",function(a1, prefix, eventIn, msg, sender)
+
+	if eventIn ~= "ASMSG_HEADHUNTING_IS_PLAYER_WANTED" then return end
+
+	local wantedStorage 	= C_CacheInstance:Get("ASMSG_HEADHUNTING_IS_PLAYER_WANTED", {})
+	local messageStorage 	= C_Split(msg, ",")
+	local GUID 				= tonumber(messageStorage[E_HEADHUNTING_PLAYER_IS_WANTED.GUID])
+	local isWanted 			= messageStorage[E_HEADHUNTING_PLAYER_IS_WANTED.ISWANTED] and tonumber(messageStorage[E_HEADHUNTING_PLAYER_IS_WANTED.ISWANTED])
+
+	if not isWanted then
+		return
+	end
+	if not GUID then return end
+	wantedStorage[GUID] = isWanted == 1
+
+	for _, frame in ipairs(frames) do
+		UF:Update_HeadHuntingWanted(frame, true)
+	end
+end)
+
+local function PostUpdate(frame, e)
+	if e == "OnShow" or e == "PLAYER_TARGET_CHANGED" then
+		UF:Update_HeadHuntingWanted(frame)
+	end
+end
+
+function UF:Construct_HeadHuntingWanted(frame)
+	frame.PostUpdate = PostUpdate
+
+	local wantedFrame = CreateFrame("PlayerModel", nil, frame.RaisedElementParent)
+	wantedFrame:SetSize(100, 100)
+	wantedFrame:SetPoint("CENTER", frame.Health)
+
+	tinsert(frames, frame)
+	return wantedFrame
+end
 
 function UF:Update_HeadHuntingWanted(frame, dontSendRequest)
 	local isWanted
@@ -57,39 +94,3 @@ function UF:Update_HeadHuntingWanted(frame, dontSendRequest)
 		frame.HeadHuntingWantedFrame:Hide()
 	end
 end
-local function PostUpdate(frame, e)
-	if e == "OnShow" or e == "PLAYER_TARGET_CHANGED" then
-		UF:Update_HeadHuntingWanted(frame)
-	end
-end
-
-function UF:Construct_HeadHuntingWanted(frame)
-	frame.PostUpdate = PostUpdate
-
-	local wantedFrame = CreateFrame("PlayerModel", nil, frame.RaisedElementParent)
-	wantedFrame:SetSize(100, 100)
-	wantedFrame:SetPoint("CENTER", frame.Health)
-
-	tinsert(frames, frame)
-	return wantedFrame
-end
-
-
-event:SetScript("OnEvent",function(a1, prefix, eventIn, msg, sender)
-	if not eventIn == "ASMSG_HEADHUNTING_IS_PLAYER_WANTED" then return end
-
-	local wantedStorage 	= C_CacheInstance:Get("ASMSG_HEADHUNTING_IS_PLAYER_WANTED", {})
-	local messageStorage 	= C_Split(msg, ",")
-	local GUID 				= tonumber(messageStorage[E_HEADHUNTING_PLAYER_IS_WANTED.GUID])
-	local isWanted 			= messageStorage[E_HEADHUNTING_PLAYER_IS_WANTED.ISWANTED] and tonumber(messageStorage[E_HEADHUNTING_PLAYER_IS_WANTED.ISWANTED])
-
-	if not isWanted then
-		return
-	end
-	if not GUID then return end
-	wantedStorage[GUID] = isWanted == 1
-
-	for _, frame in ipairs(frames) do
-		UF:Update_HeadHuntingWanted(frame, true)
-	end
-end)
