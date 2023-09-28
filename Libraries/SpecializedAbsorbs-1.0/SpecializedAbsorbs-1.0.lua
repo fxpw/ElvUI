09b0e9ca828b0e23baa8318c9f97fce7ffa32e16
fxpw <fxpw@outlook.com>
2023-09-29 00:22:46 +0300

    fix

--- Libraries/SpecializedAbsorbs-1.0/SpecializedAbsorbs-1.0.lua
+++ Libraries/SpecializedAbsorbs-1.0/SpecializedAbsorbs-1.0.lua
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------
 local _,ns = ...
 local Compat = ns.Compat
-local MAJOR, MINOR = "SpecializedAbsorbs-1.0", 11
+local MAJOR, MINOR = "SpecializedAbsorbs-1.0", 12
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 local Core
@@ -1723,15 +1723,16 @@ local function Tanks_CLEU(self,...)
 		-- UnitStatsTable[playerid][8] = GetShieldBlock()
 		-- UnitStatsTable[playerid][9] = GetBlockChance()
 
-		local blockValue = UnitStatsIndex(whoguid4,8)>0 and UnitStatsIndex(whoguid4,8) or 3800
-		local blockChance = UnitStatsIndex(whoguid4,9)>0 and UnitStatsIndex(whoguid4,9) or 35
-		local parryChance = UnitStatsIndex(whoguid4,10)> 0 and UnitStatsIndex(whoguid4,10) or 30
+		-- local blockValue = UnitStatsIndex(whoguid4,8)>0 and UnitStatsIndex(whoguid4,8) or 3800
+		local blockChance = UnitStatsIndex(whoguid4, 9)>0 and UnitStatsIndex(whoguid4, 9) or 35
+		local parryChance = UnitStatsIndex(whoguid4, 10)> 0 and UnitStatsIndex(whoguid4, 10) or 15
 		if (whoguid4 == PlayerGUID) then
-			blockValue = GetShieldBlock()
+			-- blockValue = GetShieldBlock()
 			blockChance = GetBlockChance()
 			parryChance = GetParryChance()
 		end
-		local absorb = (blockChance+parryChance*4) * blockValue
+
+		local absorb = (( blockChance + parryChance) * 4) * blockChance
 		if absorb > 32500 then
 			absorb = 32500
 		end
