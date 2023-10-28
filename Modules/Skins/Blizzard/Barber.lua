09adbaa331c77844072ff6e7014bebc93424eae2
fxpw <fxpw@outlook.com>
2023-10-28 10:06:25 +0300

    fix barber

--- Modules/Skins/Blizzard/Barber.lua
+++ Modules/Skins/Blizzard/Barber.lua
@@ -7,23 +7,23 @@ local S = E:GetModule("Skins")
 S:AddCallbackForAddon("Blizzard_BarbershopUI", "Skin_Blizzard_BarbershopUI", function()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.barber then return end
 
-	BarberShopFrame:CreateBackdrop("Transparent")
-	BarberShopFrame.backdrop:Point("TOPLEFT", 44, -70)
-	BarberShopFrame.backdrop:Point("BOTTOMRIGHT", -38, 42)
+	-- BarberShopFrame:CreateBackdrop("Transparent")
+	-- BarberShopFrame.backdrop:Point("TOPLEFT", 44, -70)
+	-- BarberShopFrame.backdrop:Point("BOTTOMRIGHT", -38, 42)
 
-	S:SetBackdropHitRect(BarberShopFrame)
+	-- S:SetBackdropHitRect(BarberShopFrame)
 
-	BarberShopFrameBackground:Kill()
+	-- BarberShopFrameBackground:Kill()
 
-	for i = 1, 4 do
-		S:HandleNextPrevButton(_G["BarberShopFrameSelector"..i.."Prev"])
-		S:HandleNextPrevButton(_G["BarberShopFrameSelector"..i.."Next"])
-	end
+	-- for i = 1, 4 do
+	-- 	S:HandleNextPrevButton(_G["BarberShopFrameSelector"..i.."Prev"])
+	-- 	S:HandleNextPrevButton(_G["BarberShopFrameSelector"..i.."Next"])
+	-- end
 
-	BarberShopFrameMoneyFrame:StripTextures()
-	BarberShopFrameMoneyFrame:CreateBackdrop()
+	-- BarberShopFrameMoneyFrame:StripTextures()
+	-- BarberShopFrameMoneyFrame:CreateBackdrop()
 
-	S:HandleButton(BarberShopFrameOkayButton)
-	S:HandleButton(BarberShopFrameCancelButton)
-	S:HandleButton(BarberShopFrameResetButton)
+	-- S:HandleButton(BarberShopFrameOkayButton)
+	-- S:HandleButton(BarberShopFrameCancelButton)
+	-- S:HandleButton(BarberShopFrameResetButton)
 end)
