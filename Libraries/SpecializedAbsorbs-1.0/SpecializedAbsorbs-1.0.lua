b2f2bde6615145ab9a62ea43b24c9fb90459aef2
fxpw <fxpw@outlook.com>
2024-06-12 14:54:48 +0300

    update version

--- Libraries/SpecializedAbsorbs-1.0/SpecializedAbsorbs-1.0.lua
+++ Libraries/SpecializedAbsorbs-1.0/SpecializedAbsorbs-1.0.lua
@@ -3,7 +3,7 @@
 ------------------------------------------------------------------------
 local _,ns = ...
 local Compat = ns.Compat
-local MAJOR, MINOR = "SpecializedAbsorbs-1.0", 18
+local MAJOR, MINOR = "SpecializedAbsorbs-1.0", 19
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR)
 if not lib then return end
 local Core
