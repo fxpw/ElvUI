2340fbc114e4cfd6a63ef9de55efe7834c4d700e
fxpw <fxpw@outlook.com>
2024-07-20 22:01:44 +0300

    typo

--- Modules/Chat/Chat.lua
+++ Modules/Chat/Chat.lua
@@ -344,7 +344,7 @@ local specialChatIconsSirus = {
 	["Thornmuse-ProxyEU Scourge x2 - 3.3.5a+"] = thorncrown,
 	["Thornmuse-Scourge x2 - 3.3.5a+"] = thorncrown,
 	["Luana-Scourge x2 - 3.3.5a+"] = lun,
-	["Luana-ProxyEU Scourge x2 - 3.3.5a+"] = thorncrown,
+	["Luana-ProxyEU Scourge x2 - 3.3.5a+"] = lun,
 
 
 }
