91142af7184b8d0f618b65cc76a741acdd8775f6
Vladimir <vl4d1m1r0ff@gmail.com>
2025-05-07 07:20:04 +0700

    Замена пробелов на табы

--- Modules/Skins/Blizzard_Sirus/Sirus_LegacyCollection.lua
+++ Modules/Skins/Blizzard_Sirus/Sirus_LegacyCollection.lua
@@ -5,39 +5,39 @@ local S = E:GetModule("Skins")
 --WoW API / Variables
 
 local function AllStrip(self)
-    self.TopLeft:Hide();
-    self.TopRight:Hide();
-    self.BottomLeft:Hide();
-    self.BottomRight:Hide();
-    self.TopMiddle:Hide();
-    self.MiddleLeft:Hide();
-    self.MiddleRight:Hide();
-    self.BottomMiddle:Hide();
-    self.MiddleMiddle:Hide();
+	self.TopLeft:Hide();
+	self.TopRight:Hide();
+	self.BottomLeft:Hide();
+	self.BottomRight:Hide();
+	self.TopMiddle:Hide();
+	self.MiddleLeft:Hide();
+	self.MiddleRight:Hide();
+	self.BottomMiddle:Hide();
+	self.MiddleMiddle:Hide();
 end
 local function LoadSkin()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.legacyCollection then return end
 	S:HandleDropDownBox(HeirloomsJournalClassDropDown);
 	S:HandleTab(CollectionsJournalTab5)
 	HeirloomsJournalIconsFrameOverlayFrame:StripTextures()
-    HeirloomsJournal:StripTextures()
+	HeirloomsJournal:StripTextures()
 	HeirloomsJournalIconsFrame:StripTextures()
-    HeirloomsJournalIconsFrame:CreateBackdrop("Default")
-    AllStrip(HeirloomsJournalFilterButton)
-    S:HandleButton(HeirloomsJournalFilterButton)
-    S:HandleEditBox(HeirloomsJournalSearchBox)
-    S:HandleNextPrevButton(HeirloomsJournalNextPageButton,"right")
-    S:HandleNextPrevButton(HeirloomsJournalPrevPageButton,"left")
-    S:HandleStatusBar(HeirloomsJournalProgressBar)
+	HeirloomsJournalIconsFrame:CreateBackdrop("Default")
+	AllStrip(HeirloomsJournalFilterButton)
+	S:HandleButton(HeirloomsJournalFilterButton)
+	S:HandleEditBox(HeirloomsJournalSearchBox)
+	S:HandleNextPrevButton(HeirloomsJournalNextPageButton,"right")
+	S:HandleNextPrevButton(HeirloomsJournalPrevPageButton,"left")
+	S:HandleStatusBar(HeirloomsJournalProgressBar)
 
 	HeirloomsJournalIconsFrame:HookScript("OnShow",function(self)
-        for i = 1,18 do
-            local button = _G["HeirloomsJournalSpellButton"..i]
-            if button and button.Cooldown then
-                E:RegisterCooldown(button.Cooldown)
-            end
-        end
-    end)
+		for i = 1,18 do
+			local button = _G["HeirloomsJournalSpellButton"..i]
+			if button and button.Cooldown then
+				E:RegisterCooldown(button.Cooldown)
+			end
+		end
+	end)
 end
 
 
