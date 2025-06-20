404e9636b91d891aeb65d695f1d78d98ebd910a5
Vladimir <vl4d1m1r0ff@gmail.com>
2025-06-21 03:24:50 +0700

    Пробелы заменил на табы

--- Modules/DataTexts/ArmorPenetration.lua
+++ Modules/DataTexts/ArmorPenetration.lua
@@ -2,7 +2,6 @@ local E, L, V, P, G = unpack(select(2, ...))
 local DT = E:GetModule("DataTexts")
 
 local strjoin = strjoin
-local format = format
 
 local displayString = ''
 local APRating = 0
@@ -11,15 +10,10 @@ local function OnEvent(self)
 	APRating = GetCombatRating(25)
 
 	self.text:SetFormattedText(displayString, APRating)
-
-	lastPanel = self
 end
 
 local function ApplySettings(_, hex)
 	displayString = strjoin('', 'РПБ: ', '%d')
-	if lastPanel ~= nil then
-		OnEvent(lastPanel)
-	end
 end
 
 E.valueColorUpdateFuncs[ApplySettings] = true
