feff17e344b1fba7ed9c35331ea41a5d699d84ef
fxpw <fxpw@outlook.com>
2025-07-19 05:49:43 +0300

    green light

--- Modules/Skins/Blizzard/WatchFrame.lua
+++ Modules/Skins/Blizzard/WatchFrame.lua
@@ -2,14 +2,13 @@ local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateD
 local S = E:GetModule("Skins")
 
 --Lua functions
-local _G = _G
-local unpack = unpack
-local format = string.format
+-- local _G = _G
+-- local unpack = unpack
+-- local format = string.format
 --WoW API / Variables
 
 local function LoadSkin()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.watchframe or not WatchFrame then return end
-
 
 end
 
