7dac4f8e07ecd45d7971c2a7468fa6c3ddf2ebd5
Accidev <accidev@yandex.ru>
2026-01-20 17:47:24 +0300

    typo

--- Modules/Skins/Blizzard/PromoCode.lua
+++ Modules/Skins/Blizzard/PromoCode.lua
@@ -1,9 +1,6 @@
 local E, L, V, P, G = unpack(select(2, ...)) --Import: Engine, Locales, PrivateDB, ProfileDB, GlobalDB
 local S = E:GetModule("Skins")
 
-local _G = _G
-local unpack = unpack
-
 local function LoadSkin()
 	if not E.private.skins.blizzard.enable or not E.private.skins.blizzard.misc then return end
 
